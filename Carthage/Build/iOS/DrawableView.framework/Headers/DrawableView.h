810b7c7f84124cf00f71ac22c771df5af4ea262a
ethanschatzline <ethanschatzline@yahoo.com>
2017-04-13 00:38:28 -0700

    build podspec and carthage file

---
+++ Carthage/Build/iOS/DrawableView.framework/Headers/DrawableView.h
@@ -0,0 +1,19 @@
+//
+//  DrawableView.h
+//  DrawableView
+//
+//  Created by Ethan Schatzline on 4/9/17.
+//  Copyright © 2017 Ethan Schatzline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DrawableView.
+FOUNDATION_EXPORT double DrawableViewVersionNumber;
+
+//! Project version string for DrawableView.
+FOUNDATION_EXPORT const unsigned char DrawableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DrawableView/PublicHeader.h>
+
+
