c9501cdbd5a94661b74a82cfa2b1d1c754dc897a
ethanschatzline <ethanschatzline@yahoo.com>
2017-04-20 11:03:36 -0700

    shorten tests

--- DrawableViewTests/LatestStrokeCollectionTests.swift
+++ DrawableViewTests/LatestStrokeCollectionTests.swift
@@ -17,6 +17,7 @@ class LatestStrokeCollectionTests: XCTestCase {
         super.setUp()
         
         latestStrokeCollection = LatestStrokeCollection()
+        testIsEmptyStrokeCollection()
     }
     
     override func tearDown() {
@@ -25,124 +26,50 @@ class LatestStrokeCollectionTests: XCTestCase {
         latestStrokeCollection.clear()
     }
     
-    func testNewStroke() {
+    func testIsEmptyStrokeCollection() {
+        XCTAssertNil(latestStrokeCollection.lastStroke)
+        XCTAssertNil(latestStrokeCollection.lastBrush)
+        XCTAssertNil(latestStrokeCollection.lastPoint)
+        XCTAssertEqual(latestStrokeCollection.totalPointCount, 0)
+        XCTAssertEqual(latestStrokeCollection.strokeCount, 0)
+        XCTAssertTrue(latestStrokeCollection.isEmpty)
         XCTAssertEqual(latestStrokeCollection.transferrablePointCount, 0)
-        
+    }
+    
+    func testNewStroke() {
         let transparentBrush = Brush(width: 4.0, color: .red, transparency: 0.5)
         latestStrokeCollection.newStroke(initialPoint: .zero, brush: transparentBrush)
-        
         XCTAssertEqual(latestStrokeCollection.transferrablePointCount, 0)
-        
         latestStrokeCollection.newStroke(initialPoint: .zero, brush: blackBrush)
-        
         XCTAssertEqual(latestStrokeCollection.transferrablePointCount, 6)
-        
-        latestStrokeCollection.newStroke(initialPoint: .zero, brush: blackBrush)
-        
-        XCTAssertEqual(latestStrokeCollection.transferrablePointCount, 9)
     }
     
     func testAddPoint() {
-        XCTAssertNil(latestStrokeCollection.lastStroke)
-        XCTAssertNil(latestStrokeCollection.lastBrush)
-        XCTAssertNil(latestStrokeCollection.lastPoint)
-        XCTAssertEqual(latestStrokeCollection.totalPointCount, 0)
-        XCTAssertEqual(latestStrokeCollection.strokeCount, 0)
-        XCTAssertTrue(latestStrokeCollection.isEmpty)
-        XCTAssertEqual(latestStrokeCollection.transferrablePointCount, 0)
-        
-        latestStrokeCollection.addPoint(.zero)
-        
-        XCTAssertNil(latestStrokeCollection.lastStroke)
-        XCTAssertNil(latestStrokeCollection.lastBrush)
-        XCTAssertNil(latestStrokeCollection.lastPoint)
-        XCTAssertEqual(latestStrokeCollection.totalPointCount, 0)
-        XCTAssertEqual(latestStrokeCollection.strokeCount, 0)
-        XCTAssertTrue(latestStrokeCollection.isEmpty)
-        XCTAssertEqual(latestStrokeCollection.transferrablePointCount, 0)
-        
         latestStrokeCollection.newStroke(initialPoint: .zero, brush: blackBrush)
-        
-        XCTAssertEqual(latestStrokeCollection.totalPointCount, 3)
-        XCTAssertEqual(latestStrokeCollection.transferrablePointCount, 3)
-        
         let point = CGPoint(x: 10, y: 10)
         latestStrokeCollection.addPoint(point)
-        
-        XCTAssertEqual(latestStrokeCollection.totalPointCount, 4)
-        XCTAssertEqual(latestStrokeCollection.lastPoint, point)
-        XCTAssertEqual(latestStrokeCollection.transferrablePointCount, 4)
-        
         // Create a point with a distance exactly equal to 1.5 from the point we just added
+        // So that it is NOT added
         let secondPoint = CGPoint(x: 11.060660171779821, y: 11.060660171779821)
         latestStrokeCollection.addPoint(secondPoint)
-        
-        XCTAssertEqual(latestStrokeCollection.totalPointCount, 4)
-        XCTAssertNotEqual(latestStrokeCollection.lastPoint, secondPoint)
-        XCTAssertEqual(latestStrokeCollection.lastPoint, point)
         XCTAssertEqual(latestStrokeCollection.transferrablePointCount, 4)
     }
     
     func testRemoveLastStroke() {
-        XCTAssertNil(latestStrokeCollection.lastStroke)
-        XCTAssertNil(latestStrokeCollection.lastBrush)
-        XCTAssertNil(latestStrokeCollection.lastPoint)
-        XCTAssertEqual(latestStrokeCollection.totalPointCount, 0)
-        XCTAssertEqual(latestStrokeCollection.strokeCount, 0)
-        XCTAssertTrue(latestStrokeCollection.isEmpty)
-        XCTAssertEqual(latestStrokeCollection.transferrablePointCount, 0)
-        
         latestStrokeCollection.newStroke(initialPoint: .zero, brush: blackBrush)
-        
-        XCTAssertNotNil(latestStrokeCollection.lastStroke)
-        XCTAssertNotNil(latestStrokeCollection.lastBrush)
-        XCTAssertNotNil(latestStrokeCollection.lastPoint)
-        XCTAssertEqual(latestStrokeCollection.totalPointCount, 3)
-        XCTAssertEqual(latestStrokeCollection.transferrablePointCount, 3)
-        
         latestStrokeCollection.removeLastStroke()
-        
-        XCTAssertNil(latestStrokeCollection.lastStroke)
-        XCTAssertNil(latestStrokeCollection.lastBrush)
-        XCTAssertNil(latestStrokeCollection.lastPoint)
-        XCTAssertEqual(latestStrokeCollection.totalPointCount, 0)
-        XCTAssertEqual(latestStrokeCollection.strokeCount, 0)
-        XCTAssertTrue(latestStrokeCollection.isEmpty)
-        XCTAssertEqual(latestStrokeCollection.transferrablePointCount, 0)
+        testIsEmptyStrokeCollection()
     }
     
     func testClear() {
-        XCTAssertNil(latestStrokeCollection.lastStroke)
-        XCTAssertNil(latestStrokeCollection.lastBrush)
-        XCTAssertNil(latestStrokeCollection.lastPoint)
-        XCTAssertEqual(latestStrokeCollection.totalPointCount, 0)
-        XCTAssertEqual(latestStrokeCollection.strokeCount, 0)
-        XCTAssertTrue(latestStrokeCollection.isEmpty)
-        XCTAssertEqual(latestStrokeCollection.transferrablePointCount, 0)
-        
         latestStrokeCollection.newStroke(initialPoint: .zero, brush: blackBrush)
         latestStrokeCollection.newStroke(initialPoint: .zero, brush: blackBrush)
-        
-        XCTAssertNotNil(latestStrokeCollection.lastStroke)
-        XCTAssertNotNil(latestStrokeCollection.lastBrush)
-        XCTAssertNotNil(latestStrokeCollection.lastPoint)
-        XCTAssertEqual(latestStrokeCollection.totalPointCount, 6)
-        XCTAssertEqual(latestStrokeCollection.transferrablePointCount, 6)
-        
         latestStrokeCollection.clear()
-        
-        XCTAssertNil(latestStrokeCollection.lastStroke)
-        XCTAssertNil(latestStrokeCollection.lastBrush)
-        XCTAssertNil(latestStrokeCollection.lastPoint)
-        XCTAssertEqual(latestStrokeCollection.totalPointCount, 0)
-        XCTAssertEqual(latestStrokeCollection.strokeCount, 0)
-        XCTAssertTrue(latestStrokeCollection.isEmpty)
-        XCTAssertEqual(latestStrokeCollection.transferrablePointCount, 0)
+        testIsEmptyStrokeCollection()
     }
     
     func testSplitInTwoIsEmpty() {
         let emptyCollection = latestStrokeCollection.splitInTwo(numPoints: 1000)
-        
         XCTAssertNil(emptyCollection.lastStroke)
         XCTAssertNil(emptyCollection.lastBrush)
         XCTAssertNil(emptyCollection.lastPoint)
@@ -154,12 +81,7 @@ class LatestStrokeCollectionTests: XCTestCase {
     func testSplitInTwoTransfersStroke() {
         latestStrokeCollection.newStroke(initialPoint: .zero, brush: blackBrush)
         latestStrokeCollection.newStroke(initialPoint: .zero, brush: blackBrush)
-        
-        XCTAssertEqual(latestStrokeCollection.totalPointCount, 6)
-        XCTAssertEqual(latestStrokeCollection.transferrablePointCount, 6)
-        
         let newCollection = latestStrokeCollection.splitInTwo(numPoints: 500)
-        
         XCTAssertEqual(latestStrokeCollection.totalPointCount, 1)
         XCTAssertEqual(latestStrokeCollection.transferrablePointCount, 1)
         XCTAssertEqual(newCollection.totalPointCount, 6)
@@ -167,12 +89,7 @@ class LatestStrokeCollectionTests: XCTestCase {
     
     func testSplitInTwoSplitsStroke() {
         latestStrokeCollection.newStroke(initialPoint: .zero, brush: blackBrush)
-        
-        XCTAssertEqual(latestStrokeCollection.totalPointCount, 3)
-        XCTAssertEqual(latestStrokeCollection.transferrablePointCount, 3)
-        
         let newCollection = latestStrokeCollection.splitInTwo(numPoints: 2)
-        
         XCTAssertEqual(latestStrokeCollection.transferrablePointCount, 2)
         XCTAssertEqual(latestStrokeCollection.totalPointCount, 2)
         XCTAssertEqual(newCollection.totalPointCount, 2)
